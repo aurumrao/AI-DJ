b2dd0cfee29573404e65c4b3b121de484d7e1ffa
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-03-30 08:59:07 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 song = "";
-
+song2 = "";
 function setup() {
     canvas = createCanvas(500,400);
     canvas.position(437, 165);
@@ -13,17 +13,24 @@ function draw(){
 }
 
 function preload(){
-    song = loadSound("music (2).mp3");
+    song = loadSound("Song1.mp3");
+    song2 = loadSound("Song2.mp3");
 }
 
 function play(){
     song.play();
 }
-
+function play2(){
+    song2.play();
+}
 function pause(){
     song.pause();
+}
+function pause2(){
+    song2.pause();
 }
 
 function stop(){
     song.stop();
+    song2.stop();
 }
